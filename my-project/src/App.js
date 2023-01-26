84d6bfabda1e5b96be2f9002cedf4b0f88811e4c
mamantube <firmanhiday.id007@gmail.com>
2023-01-26 13:15:16 +0700

    add react app

---
+++ my-project/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Home from "./components/Home";
+import About from "./components/About"
+import Project from "./components/Project"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Project />
+    </div>
+  );
+}
+
+export default App;
